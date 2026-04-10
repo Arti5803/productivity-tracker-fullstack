1f760845f18fdfa7c3def70bfa48826589e3201b
Arti Lodhi <al4924693@gmail.com>
2026-04-10 10:35:48 +0530

    fix port for render

--- backend/server.js
+++ backend/server.js
@@ -52,8 +52,10 @@ app.get('/', (req, res) => {
   res.send('Backend is running 🚀');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 //delete API
 app.delete('/tasks/:id', async (req, res) => {
